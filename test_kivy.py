8e588aa5f503b75bd8f171b87739f44b6b649b9b
gmgniap <thetpaimyo103@gmail.com>
2019-07-04 15:41:50 +0630

    Login Screen

--- test_kivy.py
+++ test_kivy.py
@@ -1,10 +1,21 @@
 import kivy
-from kivy.app import App 
-from kivy.uix.label import Label 
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+
+class LoginScreen(GridLayout):
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        self.cols = 2
+
+        self.add_widget(Label(text="Username"))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
 
 class Test(App):
     def build(self):
-        return Label(text='Hiiiii')
+        return LoginScreen()
 
 if __name__ == "__main__":
     Test().run()
